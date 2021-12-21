8273fe084f64cf30ff31af9205da63bc32b89cea
kapza <66725494+BitterAlmondJuice@users.noreply.github.com>
2021-12-21 17:05:54 +0100

    Update Persona.java

--- Persona.java
+++ Persona.java
@@ -39,26 +39,39 @@ public class Persona {
         this.genero = genero;
     }
 
-    public String getNombre(){
+    public String getNombre() {
+
         return nombre;
     }
-    public int getEdad(){
+
+    public int getEdad() {
+
         return edad;
     }
-    public double getAltura(){
+
+    public double getAltura() {
+
         return altura;
     }
-    public double getPeso(){
+
+    public double getPeso() {
+
         return peso;
     }
-    public char getGenero(){
+
+    public char getGenero() {
+
         return genero;
     }
-    public double getImc(){
-        this.imc = peso / (altura*altura);
+
+    public double getImc() {
+
+        this.imc = peso / (altura * altura);
         return imc;
     }
-    public String getEstado(){
+
+    public String getEstado() {
+
         double imc = getImc();
         if (imc < 10.5) this.estado = "Peso inferior al normal, ¿quieres unas lentejas?";
         if (imc >= 18.5 && imc < 24.9) this.estado = "Peso normal";
